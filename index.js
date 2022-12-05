56723601d07b9c03e2f10ea22aafa4bdcc4653cc
Jay <Jay@macbook>
2022-12-05 11:11:07 +0800

    Initial setting (Louisa Cafe - safe study)

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const userRoute = require('./routes/user');
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log('db connected'))
+    .catch((err) => console.log(err));
+
+app.use('/api/user', userRoute);
+
+const port = process.env.PORT || 5001;
+app.listen(port, () => {
+    console.log(`Server is running in ${port} port`);
+});
